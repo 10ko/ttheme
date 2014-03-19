216ebefa8a66e3ed165ec0328e68d6708d443280
Emanuele Libralato <tenko@tenkobox.local>
2014-03-19 22:26:25 +0000

    Added foundation to project

--- footer.php
+++ footer.php
@@ -2,10 +2,17 @@
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'ttheme' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'ttheme' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
 </div>
 </footer>
 </div>
+
+
+<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/foundation.min.js"></script>
+<script>
+  $(document).foundation();
+</script>
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php wp_title( ' | ', true, 'right' ); ?></title>
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/foundation.css" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
 <section id="branding">
-<div id="site-title"><?php if ( ! is_singular() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( ! is_singular() ) { echo '</h1>'; } ?></div>
+<div id="site-title"><?php if ( ! is_singular() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'ttheme' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( ! is_singular() ) { echo '</h1>'; } ?></div>
 <div id="site-description"><?php bloginfo( 'description' ); ?></div>
 </section>
 <nav id="menu" role="navigation">
@@ -21,4 +22,4 @@
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 </nav>
 </header>
-<div id="container">
+<div class="row" id="container">
