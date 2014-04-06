386548a9d38cf24aa80079ebdde2d53bfd023ffe
tenko <emanuele@elibralato.com>
2014-04-06 14:30:48 +0200

    Added basic foundation grid to pages and single post

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="clear"></div>
 </div>
-<footer id="footer" role="contentinfo">
+<footer id="footer" role="contentinfo" class="columns small-centered small-6">
 <div id="copyright">
 <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'ttheme' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
 </div>
--- single.php
+++ single.php
@@ -1,12 +1,7 @@
 <?php get_header(); ?>
-<section id="content" role="main">
+<section id="content" role="main" class="columns small-centered small-6">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+<?php get_template_part( 'entry','content' ); ?>
 <?php endwhile; endif; ?>
-<footer class="footer">
-<?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
 </section>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
